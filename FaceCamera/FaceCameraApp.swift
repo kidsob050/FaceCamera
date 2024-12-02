03c4f60d084595da39224352f872e19f3826f2ab
kidsob050 <kidsob7872@gmail.com>
2024-12-02 13:35:53 +0800

    Initial Commit

---
+++ FaceCamera/FaceCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceCameraApp.swift
+//  FaceCamera
+//
+//  Created by user on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
